9074926fa574eb8b139671995f444a6356b7f3d8
Muhamad Bagus Septian <bagusseptian21@gmail.com>
2022-12-19 03:48:43 +0700

    update project

--- Burn_Rate_Prediction/deployment/backend/app.py
+++ Burn_Rate_Prediction/deployment/backend/app.py
@@ -31,21 +31,34 @@ def burnout_inference(data, model=burnout_model):
     data = pd.DataFrame(data, columns=columns)
     res = model.predict(data)
     return res[0], value[res[0]]
-    
 
-@app.route("/burnout", methods=['POST'])
+@app.route("/burnout")
 def burnout_predict():
-    args = request.json
-    gender = args.get("gender")
-    wfh = args.get("wfh")
-    designation = args.get("designation")
-    resource = args.get("resource")
-    mental_score = args.get("mental_score")
+    args = request.args
+    gender = args.get("gender", type=object, default='Male')
+    wfh = args.get("wfh", type=object, default='No')
+    designation = args.get("designation", type=int, default=0)
+    resource = args.get("resource", type=int, default=1)
+    mental_score = args.get("mental_score", type=float, default=0)
     new_data = [gender, wfh, designation,
                 resource, mental_score]
     value = burnout_inference(new_data)
-    response = jsonify(value)
-    return response
+    response = jsonify(result=(round(float(value),2)))
+    return response    
+
+# @app.route("/burnout", methods=['POST'])
+# def burnout_predict():
+#    args = request.json
+#    gender = args.get("gender")
+#    wfh = args.get("wfh")
+#    designation = args.get("designation")
+#    resource = args.get("resource")
+#    mental_score = args.get("mental_score")
+#    new_data = [gender, wfh, designation,
+#                resource, mental_score]
+#    value = burnout_inference(new_data)
+#    response = jsonify(value)
+#    return response
 
 
 # app.run(debug=True)
