49a8b0309f67f80448bfac3648eb33e0d7d09d2f
Muhamad Bagus Septian <bagusseptian21@gmail.com>
2023-01-03 19:50:55 +0700

    update frontend

--- Facemask_Image_Detection/deployment/frontend/app.py
+++ Facemask_Image_Detection/deployment/frontend/app.py
@@ -1,46 +1,54 @@
 import json
 import matplotlib.pyplot as plt
-import cv2
+from PIL import Image, ImageOps
 import numpy as np
 import streamlit as st
 import requests
 
 # Set header image
-header = cv2.imread("model/facemask.jpg")
-st.image(cv2.cvtColor(header, cv2.COLOR_BGR2RGB))
+header = plt.imread("data/facemask_reg.png")
+st.image(header)
 
 # Set title
 st.title("Aplikasi Pengecekan Face Mask")
 
 # Input image
-upload = st.file_uploader('Masukkan gambar untuk pengecekan', type=['png','jpg'])
+upload = st.file_uploader('Masukkan gambar untuk pengecekan', type=['png','jpg', 'jpeg'])
 
-# Open input image
-inf = plt.imread(upload)
-# Convert image to array
-imgs = np.asarray(inf)
-# Convert input image to size
-img = cv2.resize(imgs, (150,150))
-# Expand array
-new_data = np.expand_dims(imgs, 0)
+if upload is not None:
+    image = Image.open(upload)
+    st.image(image)
 
-# Convert array to list
-new_data = new_data.tolist()
+
+    img = Image.open(upload)
+    # Define image size
+    size = (150,150)
+    # Convert input image to size
+    resize = ImageOps.fit(image, size, Image.ANTIALIAS)
+    # Convert image to array
+    imgs_array = np.asarray(resize)
+    # Expand array
+    new_data = np.expand_dims(imgs_array, 0)
+
+    # Convert array to list
+    new_data = new_data.tolist()
     
 
-# inference
-URL = 'http://face-mask-ml2p2-backend.herokuapp.com/v1/models/facemask_model:predict'
-param = json.dumps({
-        "signature_name":"serving_default",
-        "instances":new_data
-    })
-r = requests.post(URL, data=param)
+    # inference
+    URL = 'http://face-mask-ml2p2-backend.herokuapp.com/v1/models/facemask_model:predict'
+    param = json.dumps({
+            "signature_name":"serving_default",
+            "instances":new_data
+        })
+    r = requests.post(URL, data=param)
 
-if r.status_code == 200:
-    res = r.json()
-    if res['predictions'][0][0] > 0.5:
-        st.title("Maaf, Anda tidak menggunakan masker")
+    if r.status_code == 200:
+        res = r.json()
+        if res['predictions'][0][0] > 0.5:
+            st.title("Maaf, Anda tidak menggunakan masker")
+        else:
+            st.title("Terima kasih, Anda sudah menggunakan masker")
     else:
-        st.title("Terima kasih, Anda sudah menggunakan masker")
+        st.title("Unexpected Error")
 else:
-    st.title("Unexpected Error")
+    st.title("Silakan upload gambar terlebih dahulu")
